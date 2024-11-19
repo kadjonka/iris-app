a302f9cbc27e21c1932a04bc76731a55ec2ec900
kadjonka <zebkalbafaustine@gmail.com>
2024-11-19 15:51:00 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,19 +1,38 @@
 import streamlit as st
-import pandas as pd 
-#titre
+import pandas as pd
+import altair as alt
+#import matplotlib.pyplot as plt
 
-# Charger les données
-data = pd.read_csv('Iris.csv', delimiter=';')
-st.title('mon premier dashboard avec Streamlit')
-# Créer un titrest.title("Mon premier tableau de bord Streamlit") 
-# Afficher les données dans un tableau
-#st.table(data)
-         
- chart = alt.chart(data).mark_bar().encode(x='SepalLength', y='SepalWidth')
-st.altair_chart(chart, use_container_width= True)
 
+data =pd.read_csv('Iris.csv', delimiter = ';')
+st.title('Mon premier dashboard avec Streamlit')
+ #st. table(data)
+import altair as alt
+import pandas as pd
+
+   # Créer un chart Altair
+chart = alt.Chart(data).mark_bar().encode( x='SepalLength', y='SepalWidth')
+# Afficher le chart sur Streamlit
+st.altair_chart(chart, use_container_width=True)
+                                           
+chart = alt.Chart(data).mark_point().encode( x='SepalLength', y='PetalLength')
+# Afficher le chart sur Streamlit
 st.altair_chart(chart, use_container_width=True)
 
-                                                      
 
+import streamlit as st
 
+# Title for the main app
+st.title("Main App Area")
+
+# Title for the sidebar
+st.sidebar.title("Sidebar")
+
+# Add widgets to the sidebar
+option = st.sidebar.selectbox(
+    "Select an option:",
+    ["Option iris", "Option data", "Option github"]
+)
+
+# Display selected option
+st.write(f"You selected: {option}")
