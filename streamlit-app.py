e158313842dd28be8d4bd04d2827c5c0853b4606
kadjonka <zebkalbafaustine@gmail.com>
2024-11-19 14:40:32 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,8 +8,13 @@ data = pd.read_csv('Iris.csv', delimiter=';')
 # Créer un titrest.title("Mon premier tableau de bord Streamlit") 
 # Afficher les données dans un tableau
 #st.table(data)
-st.write(data.head())
-           
+st.write(data.head()
+         
+ chart = alt.chart (data). mark_bar(), encode( x='sepallenght', y='sepalwidth')
+st.altair_chart (chart, use_container_width= True)
+chart = alt.chart( data),mark_point().encode(x='sepallenght', y='sepalwidth')
+st.altair_char(chart, use contenair_width=True
 
+                                                      
 
 
