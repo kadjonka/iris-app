83577aa1e86aa058dc59242d65875d343afc1fde
kadjonka <zebkalbafaustine@gmail.com>
2024-11-19 15:22:13 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,19 +1,18 @@
 import streamlit as st
 import pandas as pd 
 #titre
-st.title('ceci est mon dashboard')
+
 # Charger les données
 data = pd.read_csv('Iris.csv', delimiter=';')
-
+st.title('mon premier dashboard avec Streamlit')
 # Créer un titrest.title("Mon premier tableau de bord Streamlit") 
 # Afficher les données dans un tableau
 #st.table(data)
-st.write(data.head()
          
- chart = alt.chart (data). mark_bar(), encode( x='sepallenght', y='sepalwidth')
-st.altair_chart (chart, use_container_width= True)
-chart = alt.chart( data),mark_point().encode(x='sepallenght', y='sepalwidth')
-st.altair_chart(chart, use contenair_width=True
+ chart = alt.chart(data).mark_bar().encode(x='SepalLength', y='SepalWidth')
+st.altair_chart(chart, use_container_width= True)
+
+st.altair_chart(chart, use_container_width=True)
 
                                                       
 
