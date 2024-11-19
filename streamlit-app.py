972b0cf1c5a2683e553cf0f4d5a19ce45e9202f4
kadjonka <zebkalbafaustine@gmail.com>
2024-11-19 14:18:10 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -11,26 +11,5 @@ data = pd.read_csv('Iris.csv', delimiter=';')
 st.write(data.head())
            
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-# Titre de l'application
-st.title('Mon application Streamlit avec graphique')
 
-# Créer des données
-data = pd.DataFrame({
-    'x': np.arange(0, 10),
-    'y': np.random.rand(10)
-})
-
-# Graphique avec Matplotlib
-plt.figure()
-plt.plot(data['x'], data['y'], marker='o')
-plt.title('Graphique Exemple')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-
-# Afficher le graphique
-st.pyplot(plt)
