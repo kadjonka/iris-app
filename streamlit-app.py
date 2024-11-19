d3693f6a253342a9c52d70be57605e3c100dfdd1
kadjonka <zebkalbafaustine@gmail.com>
2024-11-19 12:29:16 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,5 +6,5 @@ data = pd.read_csv('Iris.csv', delimiter=';')
 
 # Créer un titrest.title("Mon premier tableau de bord Streamlit") 
 # Afficher les données dans un tableau
-st.table(data)
+#st.table(data)
 st.write(data.head())
